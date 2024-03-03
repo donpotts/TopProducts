e4a52b5548fac58a7349592d6ef240ecc7683279
Don Potts <don.potts@beverycreative.com>
2024-03-03 08:49:43 -0600

    Added Swagger for release access

--- Topproducts/Program.cs
+++ Topproducts/Program.cs
@@ -159,11 +159,13 @@ builder.Services.AddScoped<ImageService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseForwardedHeaders();
